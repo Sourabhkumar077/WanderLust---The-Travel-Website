0c040b3ebb5fd9f8c5279de96e16f98dc74154d6
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-06-14 16:14:36 +0530

    Day 11 : Handled the redirecting url flaw on login from different pages

--- middleware.js
+++ middleware.js
@@ -1,8 +1,16 @@
 module.exports.isLoggedIn = (req, res, next) => {
-     console.log(req.user);
+    //  console.log(req.user);
     if (!req.isAuthenticated()) {
+        req.session.redirectUrl = req.originalUrl;  // users will be redirected to that page from they coming from
         req.flash("error", "You must be signed in first!");
         return res.redirect("/login");
+    }
+    next();
+};
+// sessions are deleted after each login so saving the redirect url in locals and using them as the middleware in login route
+module.exports.saveRedirectUrl = (req, res, next) => {
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl = req.session.redirectUrl;
     }
     next();
 };
--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const User = require("../models/user");
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware");
 
-// signup functionalty routes GET + POST
+// signup functionality routes GET + POST
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
 });
@@ -14,27 +15,40 @@ router.post("/signup", wrapAsync(async (req, res) => {
         let { username, email, password } = req.body;
         let newuser = new User({ username, email });
         let registeredUser = await User.register(newuser, password);
-        console.log(registeredUser);
-        req.flash("success", "Welcome to WanderLust");
-        res.redirect("/listings");
+        req.login(registeredUser, (err) => {
+            if (err) {
+                return next(err);
+            }
+            req.flash("success", "Welcome to WanderLust!");
+            res.redirect("/listings");
+        });
     } catch (error) {
         req.flash("error", error.message);
         res.redirect("/signup");
     }
 }));
+
 // implementing the login functionality routes
 router.get("/login", (req, res) => {
     res.render("users/login.ejs");
 });
 
-router.post("/login",
-    passport.authenticate("local", { failureRedirect: '/login', failureFlash: true }),
+router.post("/login", 
+    saveRedirectUrl,
+    passport.authenticate("local", { 
+        failureRedirect: '/login', 
+        failureFlash: true 
+    }),
     async (req, res) => {
         req.flash("success", "Welcome Back!");
-        res.redirect("/listings");
-    });
+        let redirectUrl = res.locals.redirectUrl || "/listings";
+        delete req.session.redirectUrl;  // Clear the redirect URL from session
+        res.redirect(redirectUrl);
+    }
+);
+
 // logout route
-router.get("/logout", (req, res) => {
+router.get("/logout", (req, res, next) => {
     req.logout((err) => {
         if (err) {
             return next(err);
@@ -42,9 +56,7 @@ router.get("/logout", (req, res) => {
         req.flash("success", "Goodbye!");
         res.redirect("/listings");
     });
-
 });
-
 
 // exporting the routes 
 module.exports = router;
