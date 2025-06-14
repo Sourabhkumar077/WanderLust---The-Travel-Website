a9b1fc57075cf1179feeacab8b6858d8bac125ca
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-06-14 13:25:18 +0530

    Day 11 : SignUp done

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user");
+const wrapAsync = require("../utils/wrapAsync");
 
-router.get("/signUp", (req,res)=>{
+// signup functionalty routes GET + POST
+router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
 });
+
+router.post("/signup", wrapAsync(async (req, res) => {
+    try {
+        let { username, email, password } = req.body;
+        let newuser = new User({ username, email });
+        let registeredUser = await User.register(newuser, password);
+        console.log(registeredUser);
+        req.flash("success", "Welcome to WanderLust");
+        res.redirect("/listings");
+    } catch (error) {
+        req.flash("error", error.message);
+        res.redirect("/signup");
+    }
+
+}));
 
 module.exports = router;
