12116c16c2cbb9b0bde24dfa7edfd7149cdf9ef6
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2025-08-24 12:50:44 +0530

    Database : bug fixes

--- init/index.js
+++ init/index.js
@@ -1,16 +1,15 @@
-const mongoose = require("mongoose");
-const listing = require("../models/listing");
-const initData = require("./data");
-
+import { connect } from "mongoose";
+import Listing from "../models/listing.js";
+import { data } from "./data.js";
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderLust";
 
 async function InitDb() {
-    await mongoose.connect(MONGO_URL);
-    await listing.deleteMany({});
+    await connect(MONGO_URL);
+    await Listing.deleteMany({});
     // adding the owner property into each listing 
-    initData.data = initData.data.map((obj) => ({ ...obj, owner: "684d2fe2cbdc858c554e5c88" })); 
-    await listing.insertMany(initData.data);
+    const mappedData = data.map((obj) => ({ ...obj, owner: "684d2fe2cbdc858c554e5c88" }));
+    await Listing.insertMany(mappedData);
 
     console.log("Database was initialized");
 }
